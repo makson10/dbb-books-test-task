44bb2b718b1bebff0f8c2175ed7da4e2a1b640e2
makson10 <makson94021@gmail.com>
2025-08-31 00:39:04 +0300

    feat: add webpack config for build projects

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,31 +1,57 @@
-const webpack = require('webpack');
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
 const Dotenv = require('dotenv-webpack');
+const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-    context: path.resolve(__dirname, 'libs/assets'),
-    entry: './src/index.ts',
-    target: 'node',
-    mode: 'development',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'index.js',
-    },
-    externals: [nodeExternals()],
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
+module.exports = (env) => {
+    const isProduction = typeof process.env.NODE_ENV !== 'undefined' && process.env.NODE_ENV === 'production';
+    const mode = isProduction ? 'production' : 'development';
+    const devtool = isProduction ? false : 'inline-source-map';
+
+    const libPath = path.resolve(__dirname, 'libs', 'assets', 'src', 'index.ts');
+    const folderName = path.basename(env.output.filename.replace('main.js', ''));
+
+    const isBuildingLib = env.output.filename.includes('libs');
+    const entryFileName = isBuildingLib
+        ? libPath
+        : path.resolve(__dirname, 'apps', folderName, 'src', 'main.ts');
+
+    const outputPath = isBuildingLib
+        ? path.resolve(__dirname, 'dist', 'libs')
+        : path.resolve(__dirname, 'dist', 'apps', folderName);
+
+    return {
+        mode,
+        devtool,
+        externals: [nodeExternals()],
+        entry: entryFileName,
+
+        output: {
+            path: outputPath,
+            filename: "main.js",
+        },
+
+        resolve: {
+            alias: {
+                "@lib/assets/decorators": libPath,
+                "@lib/assets/dto": libPath,
+                "@lib/assets/entities": libPath,
+                "@lib/assets/guards": libPath,
+                "@lib/assets/modules": libPath,
+                "@lib/assets": libPath,
+            },
+            extensions: ['.js', '.ts']
+        },
+
+        module: {
+            rules: [{
+                test: /.ts?$/,
                 loader: 'ts-loader',
-                exclude: /node_modules/,
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    plugins: [
-        new Dotenv({ path: '.env' }),
-    ],
+                exclude: /node_modules/
+            }]
+        },
+
+        plugins: [
+            new Dotenv({ path: path.resolve(__dirname, 'libs', 'assets', '.env') })
+        ],
+    };
 };
